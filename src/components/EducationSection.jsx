611dc5f551102b2b544385ef7427f9bbdf92b862
Achmad Raja Qodli Zaka <achmadrajaqz@gmail.com>
2025-01-02 12:02:27 +0700

    fix: add framer motion

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { EDUCATION } from '../constants';
+import { motion } from 'framer-motion';
 
 const EducationSection = () => {
     return (
         <section className="pt-8" id="education">
-            <h2 className="mb-4 text-center text-3xl font-bold">Education</h2>
+            <motion.h2 
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+                className="mb-4 text-center text-3xl font-bold">
+                    Education
+            </motion.h2>
             {EDUCATION.map((education, index) => (
-                <div key={index} className="mb-6 p-10">
-                    <h3 className="text-xl font-semibold">{education.degree}</h3>
-                    <p className="text-lg">{education.institution}</p>
-                    <p className="text-sm text-stone-300">{education.duration}</p>
-                    <p className="text-sm text-base">{education.description}</p>
-                </div>
+                <motion.div 
+                    initial={{ opacity: 0, x: -20 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: index * 0.5 }}
+                    key={index} 
+                    className="mb-6 p-10">
+                        <h3 className="text-xl font-semibold">{education.degree}</h3>
+                        <p className="text-lg">{education.institution}</p>
+                        <p className="text-sm text-stone-300">{education.duration}</p>
+                        <p className="text-sm text-base">{education.description}</p>
+                </motion.div>
             ))}
         </section>
     )
